fa53dd5f0a1d9a36900daa774a4069fcc047efb2
Bernadette Werneke <bernadettewerneke@gmail.com>
2023-06-08 10:30:27 +0200

    project installed

---
+++ Rezepte/Rezepte/RezepteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RezepteApp.swift
+//  Rezepte
+//
+//  Created by Bernadette Werneke on 08.06.23.
+//
+
+import SwiftUI
+
+@main
+struct RezepteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Rezepte/Rezepte/ViewModel/RezeptViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RezeptViewModel.swift
+//  Rezepte
+//
+//  Created by Bernadette Werneke on 08.06.23.
+//
+
+import Foundation
